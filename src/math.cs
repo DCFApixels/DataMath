857676f40ecdeb187cd6b9b494dbeb7bb8fb0d3b
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2023-04-03 19:36:01 +0800

    clear trash

--- src/math.cs
+++ src/math.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
-using UnityEngine.XR;
-
-namespace DCFApixels.DataMath
+﻿namespace DCFApixels.DataMath
 {
     public static partial class math
     {
