7dcf4959a7a6e91fe438147f649deec937f05e00
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2023-04-06 11:40:52 +0800

    fix

--- src/math/math.cs
+++ src/math/math.cs
@@ -11,7 +11,7 @@
         public const float E = 2.71828182846f;
         public const float GOLDEN_RATIO = 1.61803398875f;
 
-        public const float Deg2Rad = (float)Math.PI / 180f;
+        public const float Deg2Rad = (float)System.Math.PI / 180f;
         public const float Rad2Deg = 57.29578f;
         #endregion
 
