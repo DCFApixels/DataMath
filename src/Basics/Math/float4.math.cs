9e392d591f797f83db2e8c421e05d84a1c1238c5
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-08-18 18:57:19 +0800

    update

--- src/Basics/Math/float4.math.cs
+++ src/Basics/Math/float4.math.cs
@@ -252,6 +252,240 @@ namespace DCFApixels.DataMath
         #endregion
     }
 
+    //public static partial class DM // IVector4<float>
+    //{
+    //    #region Abs/Sign
+    //    [IN(LINE)] public static T Abs<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return New<T>(Abs(a.x), Abs(a.y), Abs(a.z), Abs(a.w)); }
+    //    [IN(LINE)] public static T Sign<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return New<T>(Sign(a.x), Sign(a.y), Sign(a.z), Sign(a.w)); }
+    //    [IN(LINE)] public static int4 Sign2Int<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return new int4(Sign2Int(a.x), Sign2Int(a.y), Sign2Int(a.z), Sign2Int(a.w)); }
+    //    #endregion
+    //
+    //    #region Round/Floor/Ceil
+    //    [IN(LINE)] public static T Round<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return New<T>(Round(a.x), Round(a.y), Round(a.z), Round(a.w)); }
+    //    [IN(LINE)] public static int4 Round2Int<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return new int4(Round2Int(a.x), Round2Int(a.y), Round2Int(a.z), Round2Int(a.w)); }
+    //    [IN(LINE)] public static T Floor<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return New<T>(Floor(a.x), Floor(a.y), Floor(a.z), Floor(a.w)); }
+    //    [IN(LINE)] public static int4 Floor2Int<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return new int4(Floor2Int(a.x), Floor2Int(a.y), Floor2Int(a.z), Floor2Int(a.w)); }
+    //    [IN(LINE)] public static T Ceil<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return New<T>(Ceil(a.x), Ceil(a.y), Ceil(a.z), Ceil(a.w)); }
+    //    [IN(LINE)] public static int4 Ceil2Int<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return new int4(Ceil2Int(a.x), Ceil2Int(a.y), Ceil2Int(a.z), Ceil2Int(a.w)); }
+    //
+    //    [EditorBrowsable(EditorBrowsableState.Never)] [IN(LINE)] public static int4 Round2UInt<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return new int4(Round2UInt(a.x), Round2UInt(a.y), Round2UInt(a.z), Round2UInt(a.w)); }
+    //    [EditorBrowsable(EditorBrowsableState.Never)] [IN(LINE)] public static int4 Floor2UInt<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return new int4(Floor2UInt(a.x), Floor2UInt(a.y), Floor2UInt(a.z), Floor2UInt(a.w)); }
+    //    [EditorBrowsable(EditorBrowsableState.Never)] [IN(LINE)] public static int4 Ceil2UInt<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return new int4(Ceil2UInt(a.x), Ceil2UInt(a.y), Ceil2UInt(a.z), Ceil2UInt(a.w)); }
+    //    #endregion
+    //
+    //    #region Clamp/Repeat/PingPong
+    //    /// <summary> Clamps the value between min and max. </summary>
+    //    [IN(LINE)] public static T Clamp<T>(T a, T min, T max, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return Max(min, Min(max, a)); }
+    //    /// <summary> Clamps the value between 0 and 1. </summary>
+    //    [IN(LINE)] public static T Clamp01<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return Clamp(a, New<T>(0f), New<T>(1f)); }
+    //    /// <summary> Clamps the value between -1 and 1. </summary>
+    //    [IN(LINE)] public static T ClampMirror1<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return Clamp(a, New<T>(-1f), New<T>(1f)); }
+    //
+    //    [IN(LINE)] public static T Repeat<T>(T a, T length, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return Clamp(Sub(a, Mul(Floor(Div(a, length)), length)), New<T>(0f), length); }
+    //    [IN(LINE)] public static T Repeat<T>(T a, T min, T max, _4_ _ = default) where T : unmanaged, IVector4Impl<float>  { return Add(Repeat(a, Sub(max, min)), min); }
+    //    [IN(LINE)] public static T Repeat01<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return Repeat(a, New<T>(1f)); }
+    //    [IN(LINE)] public static T RepeatMirror1<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return Repeat(a, New<T>(-1f), New<T>(1f)); }
+    //
+    //    [IN(LINE)] public static T PingPong<T>(T a, T length, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return Sub(length, Abs(Repeat(a, Sub(Mul(length, New<T>(2f)), length)))); }
+    //    [IN(LINE)] public static T PingPong<T>(T a, T min, T max, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return Add(PingPong(a, Sub(max, min)), min); }
+    //    [IN(LINE)] public static T PingPong01<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return PingPong(a, New<T>(1f)); }
+    //    [IN(LINE)] public static T PingPongMirror1<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return PingPong(a, New<T>(-1f), New<T>(1f)); }
+    //    #endregion
+    //
+    //    #region SmoothStep
+    //    /// <summary> Clamps the value between from and to. </summary>
+    //    [IN(LINE)]
+    //    public static T SmoothStep<T>(T from, T to, T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float>
+    //    {
+    //        var t = Clamp01(Div(Sub(a, from), Sub(to, from)));
+    //        return Mul(Mul(t, t), Sub(New<T>(3.0f), Mul(New<T>(2.0f), t))); //t * t * (3.0f - (2.0f * t));
+    //    }
+    //    /// <summary> Clamps the value between 0 and 1. </summary>
+    //    [IN(LINE)] public static T SmoothStep01<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return SmoothStep(a, New<T>(0f), New<T>(1f)); }
+    //    /// <summary> Clamps the value between -1 and 1. </summary>
+    //    [IN(LINE)] public static T SmoothStepMirror1<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return SmoothStep(a, New<T>(-1f), New<T>(1f)); }
+    //    #endregion
+    //
+    //    #region All/Any
+    //    [IN(LINE)] public static bool All<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return All(Equals<T>(a, default)); }
+    //    [IN(LINE)] public static bool Any<T>(T a, _4_ _ = default) where T : unmanaged, IVector4Impl<float> { return Any(Equals<T>(a, default)); }
+    //    #endregion
+    //
+    //    #region Min/Max/Sum
+    //    [IN(LINE)] public static T Max<T>(T a, T b) where T : unmanaged, IVector4Impl<float> { return New<T>(Max(a.x, b.x), Max(a.y, b.y), Max(a.z, b.z), Max(a.w, b.w)); }
+    //    [IN(LINE)] public static T AbsMax<T>(T a, T b) where T : unmanaged, IVector4Impl<float> { return New<T>(AbsMax(a.x, b.x), AbsMax(a.y, b.y), AbsMax(a.z, b.z), AbsMax(a.w, b.w)); }
+    //    [IN(LINE)] public static T Min<T>(T a, T b) where T : unmanaged, IVector4Impl<float> { return New<T>(Min(a.x, b.x), Min(a.y, b.y), Min(a.z, b.z), Min(a.w, b.w)); }
+    //    [IN(LINE)] public static T AbsMin<T>(T a, T b) where T : unmanaged, IVector4Impl<float> { return New<T>(AbsMin(a.x, b.x), AbsMin(a.y, b.y), AbsMin(a.z, b.z), AbsMin(a.w, b.w)); }
+    //    [IN(LINE)] public static float CMax<T>(T a) where T : unmanaged, IVector4Impl<float> { return CMax(a.x, a.y, a.z, a.w); }
+    //    [IN(LINE)] public static float CAbsMax<T>(T a) where T : unmanaged, IVector4Impl<float> { return CAbsMax(a.x, a.y, a.z, a.w); }
+    //    [IN(LINE)] public static float CMin<T>(T a) where T : unmanaged, IVector4Impl<float> { return CMin(a.x, a.y, a.z, a.w); }
+    //    [IN(LINE)] public static float CAbsMin<T>(T a) where T : unmanaged, IVector4Impl<float> { return CAbsMin(a.x, a.y, a.z, a.w); }
+    //    [IN(LINE)] public static float CSum<T>(T a) where T : unmanaged, IVector4Impl<float> { return (a.x + a.y) + (a.z + a.w); }
+    //    #endregion
+    //
+    //    #region Lerp
+    //    [IN(LINE)] public static T Lerp<T>(T start, T end, T t) where T : unmanaged, IVector4Impl<float> { return Add(start, Mul(t, Sub(end, start))); } // start + t * (end - start);
+    //    [IN(LINE)] public static T LerpClamp<T>(T start, T end, T t) where T : unmanaged, IVector4Impl<float> { return Lerp(start, end, Clamp01(t)); }
+    //    [IN(LINE)] public static T LerpRepeat<T>(T start, T end, T t) where T : unmanaged, IVector4Impl<float> { return Lerp(start, end, Repeat01(t)); }
+    //
+    //    [IN(LINE)] public static T UnLerp<T>(T start, T end, T a) where T : unmanaged, IVector4Impl<float> { return Div(Sub(a, start), Sub(end, start)); } //(a - start) / (end - start);
+    //    [IN(LINE)] public static T UnLerpClamp<T>(T start, T end, T a) where T : unmanaged, IVector4Impl<float> { return Clamp01(UnLerp(start, end, a)); }
+    //    [IN(LINE)] public static T UnLerpRepeat<T>(T start, T end, T a) where T : unmanaged, IVector4Impl<float> { return Repeat01(UnLerp(start, end, a)); }
+    //
+    //
+    //    [IN(LINE)]
+    //    public static T MoveTowards(T from, T to, float distance)
+    //    {
+    //        float4 dif = to - from;
+    //        if (Abs(dif) <= distance) { return to; }
+    //        return from + Sign(dif) * distance;
+    //    }
+    //    [IN(LINE)]
+    //    public static float4 MoveTowards(T from, T to, float distance, out float excess)
+    //    {
+    //        if (distance == 0)
+    //        {
+    //            excess = 0;
+    //            return from;
+    //        }
+    //        float4 dif = to - from;
+    //        float difpowmag = LengthSqr(dif);
+    //        if (difpowmag == 0f)
+    //        {
+    //            excess = distance;
+    //            return to;
+    //        }
+    //        float difmag = Sqrt(difpowmag);
+    //        excess = distance - difmag;
+    //        if (excess > -float.Epsilon)
+    //        {
+    //            return to;
+    //        }
+    //
+    //        return new float4(from.x + dif.x / difmag * distance, from.y + dif.y / difmag * distance, from.z + dif.z / difmag * distance, from.w + dif.w / difmag * distance);
+    //    }
+    //    [IN(LINE)]
+    //    public static float4 Remap(float4 oldStart, float4 oldEnd, float4 newStart, float4 newEnd, float4 v)
+    //    {
+    //        return Lerp(newStart, newEnd, UnLerp(oldStart, oldEnd, v));
+    //    }
+    //
+    //
+    //    [IN(LINE)]
+    //    public static float4 LerpAngle(float4 start, float4 end, float t)
+    //    {
+    //        float4 angle = Repeat(end - start, 360f);
+    //        if (angle > 180f) { angle -= 360f; }
+    //        //float angle = Repeat(end - start, -180f, 180f);
+    //        return start + angle * Clamp01(t);
+    //    }
+    //    [IN(LINE)]
+    //    public static float4 DeltaAngle(float4 current, float4 target)
+    //    {
+    //        float4 angle = Repeat(target - current, 360f);
+    //        if (angle > 180f) { angle -= 360f; }
+    //        //float angle = Repeat(end - start, -180f, 180f);
+    //        return angle;
+    //    }
+    //    [IN(LINE)]
+    //    public static float4 MoveTowardsAngle(float4 current, float4 target, float maxDelta)
+    //    {
+    //        float4 delta = DeltaAngle(current, target);
+    //        if (0f - maxDelta < delta && delta < maxDelta)
+    //        {
+    //            return target;
+    //        }
+    //        return MoveTowards(current, current + delta, maxDelta);
+    //    }
+    //    #endregion
+    //
+    //    #region Float State Checks
+    //    [IN(LINE)] public static bool4 IsNormalFloat(float4 a) { return new bool4(float.IsNormal(a.x), float.IsNormal(a.y), float.IsNormal(a.z), float.IsNormal(a.w)); }
+    //    [IN(LINE)] public static bool4 IsFinite(float4 a) { return new bool4(float.IsFinite(a.x), float.IsFinite(a.y), float.IsFinite(a.z), float.IsFinite(a.w)); }
+    //    [IN(LINE)] public static bool4 IsNaN(float4 a) { return new bool4(float.IsNaN(a.x), float.IsNaN(a.y), float.IsNaN(a.z), float.IsNaN(a.w)); }
+    //    [IN(LINE)] public static bool4 IsInfinity(float4 a) { return new bool4(float.IsInfinity(a.x), float.IsInfinity(a.y), float.IsInfinity(a.z), float.IsInfinity(a.w)); }
+    //    [IN(LINE)] public static bool4 IsNegativeInfinity(float4 a) { return new bool4(float.IsNegativeInfinity(a.x), float.IsNegativeInfinity(a.y), float.IsNegativeInfinity(a.z), float.IsNegativeInfinity(a.w)); }
+    //    [IN(LINE)] public static bool4 IsPositiveInfinity(float4 a) { return new bool4(float.IsPositiveInfinity(a.x), float.IsPositiveInfinity(a.y), float.IsPositiveInfinity(a.z), float.IsPositiveInfinity(a.w)); }
+    //    #endregion
+    //
+    //    #region Color
+    //    [IN(LINE)] public static float4 GammaToLinearSpace(float4 value) { const float Gamma = 2.2f; return Pow(value, Gamma); }
+    //    [IN(LINE)] public static float4 LinearToGammaSpace(float4 value) { const float InverseGamma = 1.0f / 2.2f; return Pow(value, InverseGamma); }
+    //    #endregion
+    //
+    //    #region Approximately
+    //    [IN(LINE)] public static bool4 Approximately(float4 a, float4 b) { return Approximately(a, b, CMax(1E-06f * Max(Abs(a), Abs(b)), FloatEpsilon * 8f)); }
+    //    [IN(LINE)] public static bool4 Approximately(float4 a, float4 b, float4 tolerance) { return Abs(b - a) < tolerance; }
+    //    #endregion
+    //
+    //    #region Other
+    //    [IN(LINE)] public static float Length(float4 a) { return Sqrt(Dot(a, a)); }
+    //    [IN(LINE)] public static float LengthSqr(float4 a) { return Dot(a, a); }
+    //    [IN(LINE)] public static float Distance(float4 a, float4 b) { return Length(b - a); }
+    //    [IN(LINE)] public static float DistanceSqr(float4 a, float4 b) { return LengthSqr(b - a); }
+    //    [IN(LINE)] public static float4 Normalize(float4 a) { return 1.0f / Sqrt(Dot(a, a)) * a; }
+    //    [IN(LINE)]
+    //    public static float4 NormalizeSafe(float4 a, float4 defaultvalue = default)
+    //    {
+    //        var len = Dot(a, a);
+    //        return Select(defaultvalue, RSqrt(len) * a, len > FloatMinNormal);
+    //    }
+    //
+    //    [IN(LINE)] public static float4 Project(float4 a, float4 ontoB) { return (Dot(a, ontoB) / Dot(ontoB, ontoB)) * ontoB; }
+    //    [IN(LINE)]
+    //    public static float4 ProjectSafe(float4 a, float4 ontoB, float4 defaultValue = default)
+    //    {
+    //        var proj = Project(a, ontoB);
+    //        return Select(defaultValue, proj, All(IsInfinity(proj)));
+    //    }
+    //    [IN(LINE)] public static float4 Reflect(float4 v, float4 n) { return v - 2f * n * Dot(v, n); }
+    //
+    //    [IN(LINE)] public static float4 Select(float4 falseValue, float4 trueValue, bool4 test) { return test ? trueValue : falseValue; }
+    //    [IN(LINE)] public static float4 Step(float4 threshold, float4 a) { return Select(0.0f, 1.0f, a >= threshold); }
+    //
+    //    /// <summary> Convert Radians to Degrees. x * 57.296~ </summary>
+    //    [IN(LINE)] public static float4 Degrees(float4 radians) { return radians * Rad2Deg; }
+    //    /// <summary> Convert Degrees to Radians. x * 0.0175~ </summary>
+    //    [IN(LINE)] public static float4 Radians(float4 degrees) { return degrees * Deg2Rad; }
+    //
+    //    [IN(LINE)] public static float4 Cos(float4 a) { return new float4(Cos(a.x), Cos(a.y), Cos(a.z), Cos(a.w)); }
+    //    [IN(LINE)] public static float4 Cosh(float4 a) { return new float4(Cosh(a.x), Cosh(a.y), Cosh(a.z), Cosh(a.w)); }
+    //    [IN(LINE)] public static float4 Acos(float4 a) { return new float4(Acos(a.x), Acos(a.y), Acos(a.z), Acos(a.w)); }
+    //    [IN(LINE)] public static float4 Sin(float4 a) { return new float4(Sin(a.x), Sin(a.y), Sin(a.z), Sin(a.w)); }
+    //    [IN(LINE)] public static float4 Sinh(float4 a) { return new float4(Sinh(a.x), Sinh(a.y), Sinh(a.z), Sinh(a.w)); }
+    //    [IN(LINE)] public static float4 Asin(float4 a) { return new float4(Asin(a.x), Asin(a.y), Asin(a.z), Asin(a.w)); }
+    //
+    //    [IN(LINE)] public static float4 Atan(float4 a) { return new float4(Atan(a.x), Atan(a.y), Atan(a.z), Atan(a.w)); }
+    //    [IN(LINE)] public static float4 Atan2(float4 a, float4 b) { return new float4(Atan2(a.x, b.x), Atan2(a.y, b.y), Atan2(a.z, b.z), Atan2(a.w, b.w)); }
+    //
+    //    [IN(LINE)] public static float Dot(float4 a, float4 b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+    //    [IN(LINE)] public static float4 Sqr(float4 a) { return a * a; }
+    //    [IN(LINE)] public static float4 Sqrt(float4 a) { return new float4(Sqrt(a.x), Sqrt(a.y), Sqrt(a.z), Sqrt(a.w)); }
+    //    [IN(LINE)] public static float4 RSqrt(float4 a) { return 1f / Sqrt(a); }
+    //    [IN(LINE)] public static float4 Pow(float4 a, float4 b) { return new float4(Pow(a.x, a.y), Pow(a.y, a.y), Pow(a.z, a.z), Pow(a.w, a.w)); }
+    //    [IN(LINE)] public static float4 Exp(float4 pow) { return new float4(Exp(pow.x), Exp(pow.y), Exp(pow.z), Exp(pow.z)); }
+    //    [IN(LINE)] public static float4 Exp2(float4 pow) { return new float4(Exp2(pow.x), Exp2(pow.y), Exp2(pow.z), Exp2(pow.z)); }
+    //    [IN(LINE)] public static float4 Exp10(float4 pow) { return new float4(Exp10(pow.x), Exp10(pow.y), Exp10(pow.z), Exp10(pow.z)); }
+    //    [IN(LINE)] public static float4 Log(float4 f, float4 p) { return new float4(Log(f.x, p.x), Log(f.y, p.y), Log(f.z, p.z), Log(f.w, p.w)); }
+    //    [IN(LINE)] public static float4 Log(float4 f) { return new float4(Log(f.x), Log(f.y), Log(f.z), Log(f.z)); }
+    //    [IN(LINE)] public static float4 Log2(float4 f) { return new float4(Log2(f.x), Log2(f.y), Log2(f.z), Log2(f.z)); }
+    //    [IN(LINE)] public static float4 Log10(float4 f) { return new float4(Log10(f.x), Log10(f.y), Log10(f.z), Log10(f.z)); }
+    //
+    //    [IN(LINE)] public static float4 Tan(float4 a) { return new float4(Tan(a.x), Tan(a.y), Tan(a.z), Tan(a.w)); }
+    //    [IN(LINE)] public static float4 Tanh(float4 a) { return new float4(Tanh(a.x), Tanh(a.y), Tanh(a.z), Tanh(a.w)); }
+    //
+    //    [IN(LINE)] public static float4 Truncate(float4 a) { return new float4(Truncate(a.x), Truncate(a.y), Truncate(a.z), Truncate(a.w)); }
+    //    #endregion
+    //
+    //
+    //
+    //
+    //    #region Hash
+    //    [IN(LINE)] public static uint UHash(float4 v) { return unchecked((uint)Hash(v)); }
+    //    [IN(LINE)] public static int Hash(float4 v) { return Hash<float4>(v); }
+    //    #endregion
+    //}
+
     public static partial class DMBasic // IVector4<float>
     {
         [IN(LINE)] public static T New<T>(float a, _4_ _ = default) where T : unmanaged, IVector4<float> { return New<T>(a, a, a, a); }
