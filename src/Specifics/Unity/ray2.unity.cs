94dd1c745a79e2becc32376bc805ce21f4f8e3b9
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-09-07 00:30:43 +0800

    update

---
+++ src/Specifics/Unity/ray2.unity.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_5_3_OR_NEWER
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace DCFApixels.DataMath
+{
+    public partial struct ray2
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator Ray2D(ray2 a) { return new Ray2D(a.src, a.dir); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator ray2(Ray2D a) { return new ray2(a.origin, a.direction); }
+    }
+}
+#endif
