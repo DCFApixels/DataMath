f8fd13bf94206f0097c9a678311068809a782411
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-09-07 00:42:27 +0800

    update

--- src/Specifics/Unity/ray3.unity.cs
+++ src/Specifics/Unity/ray3.unity.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 namespace DCFApixels.DataMath
 {
-    //public partial struct ray3
-    //{
-    //    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    //    public static implicit operator Ray(ray3 a) => new Ray(a.origin, a.direction);
-    //    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    //    public static implicit operator ray3(Ray a) => new ray3(a.origin, a.direction);
-    //}
+    public partial struct ray3
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator Ray(ray3 a) { return new Ray(a.src, a.dir); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator ray3(Ray a) { return new ray3(a.origin, a.direction); }
+    }
 }
 #endif
